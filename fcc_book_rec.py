7c0ad16ece6ed04d12d406ba6a4b16770ab5018d
Jewella Macabenta <jmacaben@uwo.ca>
2025-05-09 22:30:04 -0400

    Added comments, added csr_matrix

--- fcc_book_rec.py
+++ fcc_book_rec.py
@@ -10,7 +10,6 @@ import os
 import zipfile
 import requests
 
-
 # Cell 2 (given)
 url = "https://cdn.freecodecamp.org/project-data/books/book-crossings.zip"
 zip_path = "book-crossings.zip"
@@ -32,7 +31,6 @@ with zipfile.ZipFile(zip_path, 'r') as zip_ref:
 books_filename = 'BX-Books.csv'
 ratings_filename = 'BX-Book-Ratings.csv'
 
-
 # Cell 3 (given)
 # Import csv data into dataframes
 df_books = pd.read_csv(
@@ -53,8 +51,8 @@ df_ratings = pd.read_csv(
     usecols=['user', 'isbn', 'rating'],
     dtype={'user': 'int32', 'isbn': 'str', 'rating': 'float32'})
 
-
-# My code
+# MY CODE
+# Remove users with fewer than 200 ratings and books with fewer than 100 ratings
 user_rating_counts = df_ratings['user'].value_counts()
 users_to_keep = user_rating_counts[user_rating_counts > 200].index
 book_rating_counts = df_ratings['isbn'].value_counts()
@@ -63,31 +61,40 @@ books_to_keep = book_rating_counts[book_rating_counts > 100].index
 df_ratings = df_ratings[df_ratings['user'].isin(users_to_keep)]
 df_ratings = df_ratings[df_ratings['isbn'].isin(books_to_keep)]
 
+# Merge the filtered data
+df_merged = pd.merge(right = df_ratings, left = df_books, on = "isbn")
 
-df_merged = pd.merge(right=df_ratings, left=df_books, on="isbn")
-
+# Remove duplicates, i.e. when a user has rated the same book multiple times
 df_cleaned = df_merged.drop_duplicates(["title", "user"])
 
-df_pivot = df_cleaned.pivot(index='title', columns='user', values='rating').fillna(0)
+# Pivot the dataframe to create a matrix where rows are books, columns are users, and values are ratings
+df_pivot = df_cleaned.pivot(index = 'title', columns = 'user', values = 'rating').fillna(0)
 
-matrix = df_pivot.values
+# Create a sparse matrix from the pivoted dataframe
+matrix = csr_matrix(df_pivot.values)
 
-model_knn=NearestNeighbors(metric='cosine',algorithm='brute')
+# Develop a model that shows books that are similar to a given book using K-Nearest Neighbors
+model_knn = NearestNeighbors(metric = 'cosine', algorithm = 'brute')
 model_knn.fit(matrix)
 
 # function to return recommended books - this will be tested
 def get_recommends(book = ""):
-    x=df_pivot.loc[book].array.reshape(1, -1)
-    distances,indices=model_knn.kneighbors(x,n_neighbors=6)
-    R_books=[]
-    for distance,indice in zip(distances[0],indices[0]):
-       if distance!=0:
-          R_book=df_pivot.index[indice]
-          R_books.append([R_book,distance])
-         
-    recommended_books=[book,R_books[::-1]]
-    return recommended_books
+    # Get row
+    x = df_pivot.loc[book].array.reshape(1, -1)
 
+    # Use the trained model to find the 6 nearest neighbors (books) to the specified book
+    distances, indices = model_knn.kneighbors(x, n_neighbors = 6)
+
+    # Create a list to store the recommended books and their distances
+    recs=[]
+    for distance, indice in zip(distances[0], indices[0]):
+       if distance != 0: # Exclude the input book itself
+          rec = df_pivot.index[indice]
+          recs.append([rec, distance])
+
+    # Sort the recommended books by distance (similarity) in ascending order     
+    recommended_books = [book, recs[::-1]]
+    return recommended_books
 
 # Cell 5 (given)
 books = get_recommends("Where the Heart Is (Oprah's Book Club (Paperback))")
